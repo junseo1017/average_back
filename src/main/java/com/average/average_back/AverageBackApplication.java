e7d66c61cc6ccd128b4fc2edfa94633913d7a345
leeJS <dlwnstjzld@gmail.com>
2025-04-26 22:44:12 +0900

    initial commit

---
+++ src/main/java/com/average/average_back/AverageBackApplication.java
@@ -0,0 +1,13 @@
+package com.average.average_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AverageBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AverageBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/average/average_back/AverageBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.average.average_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AverageBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
